4efbdaded4e9811fc0789ca87ee892145b058b2d
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-04-29 15:06:16 +0200

    Store source channel as just ID and add prefix commands
    Just numeric IDs should be enough to identify a channel and it allows using the bot through DMs.
    Prefix commands are just a temporary solution until all slash commands are distributed.

--- src/main.py
+++ src/main.py
@@ -1,51 +1,51 @@
 from os import getenv
 
 from dotenv import load_dotenv
-from hikari import Intents, MessageCreateEvent, GuildChannel
-from lightbulb import BotApp, command, implements, SlashCommand, Context
+from hikari import Intents, MessageCreateEvent
+from lightbulb import BotApp, Context, PrefixCommand, SlashCommand, command, implements
 
-from chat import reset, next_message
+from chat import next_message, reset_conversation
 
 load_dotenv()
 
 token = getenv("BOT_TOKEN")
-bot = BotApp(token=token, intents=Intents.ALL)
+bot = BotApp(token=token, intents=Intents.ALL, prefix="!")
 source_channel = None
 
 
 @bot.command()
-@command("start", "Start conversation", guilds=(999740151855067216,))
-@implements(SlashCommand)
+@command("start", "Start conversation")
+@implements(SlashCommand, PrefixCommand)
 async def start(context: Context) -> None:
-    _restart_and_set_channel(context.get_channel())
+    _reset_conversation_and_set_channel(context.channel_id)
     await context.respond("Hello! How may I assist you today?")
 
 
 @bot.command()
-@command("stop", "Stops conversation", guilds=(999740151855067216,))
-@implements(SlashCommand)
+@command("stop", "Stops conversation")
+@implements(SlashCommand, PrefixCommand)
 async def stop(context: Context) -> None:
-    _restart_and_set_channel(None)
+    _reset_conversation_and_set_channel(None)
     await context.respond("Conversation stopped.")
 
 
 @bot.command()
-@command("restart", "Restarts conversation and its context", guilds=(999740151855067216,))
-@implements(SlashCommand)
+@command("restart", "Restarts conversation and its context")
+@implements(SlashCommand, PrefixCommand)
 async def restart(context: Context) -> None:
-    _restart_and_set_channel(context.get_channel())
+    _reset_conversation_and_set_channel(context.channel_id)
     await context.respond("Conversation restarted.")
 
 
-def _restart_and_set_channel(channel: GuildChannel | None) -> None:
-    reset()
+def _reset_conversation_and_set_channel(channel: int | None) -> None:
+    reset_conversation()
     global source_channel
     source_channel = channel
 
 
 @bot.listen()
 async def on_message(event: MessageCreateEvent) -> None:
-    if not event.is_human or event.get_channel() != source_channel:
+    if not event.is_human or event.content[0] == "!" or event.channel_id != source_channel:
         return
     response = next_message(event.content)
     await event.message.respond(response)
