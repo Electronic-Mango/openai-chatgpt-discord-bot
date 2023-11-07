603fb1fe33ca19ec7a9e6e36e787ad05608bd8c5
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-07 17:06:42 +0100

    Tweak splitting messages
    Prevent replacing all whitespaces (including newlines) with spaces.

--- src/sender.py
+++ src/sender.py
@@ -6,5 +6,17 @@ MAX_MESSAGE_LENGTH = int(getenv("MAX_MESSAGE_LENGTH", 2000))
 
 
 async def send(message: str, sender: Callable[[str], Awaitable[None]]) -> None:
-    for partial in wrap(message, MAX_MESSAGE_LENGTH):
-        await sender(partial)
+    if len(message) <= MAX_MESSAGE_LENGTH:
+        await sender(message.strip())
+        return
+    parts = wrap(
+        message,
+        MAX_MESSAGE_LENGTH,
+        tabsize=4,
+        break_long_words=False,
+        replace_whitespace=False,
+        break_on_hyphens=False,
+        drop_whitespace=False,
+    )
+    for partial in parts:
+        await sender(partial.strip())
